5af22c9dd1042051710a37dcbd83761765577a77
leva4 <kresimir.levarda@infinum.com>
2024-05-14 16:18:07 +0200

    Initial Commit

---
+++ navigare/navigareApp.swift
@@ -0,0 +1,17 @@
+//
+//  navigareApp.swift
+//  navigare
+//
+//  Created by Kresimir Levarda on 14.05.2024..
+//
+
+import SwiftUI
+
+@main
+struct navigareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
